65da7ac92d4478cac58f79a08dd3f1d409289d8f
Riley Miller <waskerdu@gmail.com>
2022-05-02 23:59:39 -0700

    minimum viable

--- automat.py
+++ automat.py
@@ -1,19 +1,17 @@
-from lib2to3.pgen2 import driver
-import sys, os, json
+import sys, os, json, time
+# I don't know if this is enough imports, I think we could use a couple more
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 class Automat:
     def __init__(self):
         self.running = False
-        if not os.path.exists(".secrets"):
-            with open(".secrets", "w") as f:
-                self.secrets = {}
-        else:
-            with open(".secrets") as f:
-                self.secrets = json.load(f)
-                print(self.secrets)
+        self.secrets = {}
         self.driver = None
         self.sequence = []
         self.checkpoint = 0
@@ -26,9 +24,13 @@ class Automat:
             "goto":self.goto,
             "grab":self.grab,
             "grab_x":self.grab_x,
+            "clear":self.clear,
             "text":self.text,
             "text_s":self.text_s,
             "text_i":self.text_i,
+            "select":self.select,
+            "select_v":self.select_v,
+            "select_t":self.select_t,
             "click":self.click,
             "click_x":self.click_x,
             "enter":self.enter,
@@ -43,10 +45,26 @@ class Automat:
             "reset",
             "secret",
             "quit",}
+        with open("secrets.json", "a") as f:
+            f.write("")
+        with open("secrets.json", "r") as f:
+            text = f.read()
+            if len(text) > 0:
+                self.secrets = json.loads(text)
+    def find_element(self, find_by, identifier):
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((find_by, identifier))
+            )
+            return element
+        except:
+            return None
     def start(self, url):
         self.sequence = self.sequence[self.checkpoint:]
-        self.driver.get(url)
-        self.run_seq(self.sequence)
+        self.goto(url)
+        # if len(self.sequence) == 0:
+        #     self.sequence.append(f"goto {url}")
+        # self.run_seq(self.sequence)
     def save(self, name):
         with open(f"sequences/{name}.mat", "w") as f:
             f.write("\n".join(self.sequence))
@@ -57,37 +75,51 @@ class Automat:
         self.secrets[name] = val
     def quit(self, _):
         self.running = False
-        if self.driver:
+        if self.driver != None:
+            # quit isn't working God knows why
             self.driver.quit()
     def goto(self, url):
         self.driver.get(url)
+        if self.find_element(By.CSS_SELECTOR, "html"):
+            pass
+        else:
+            print("url timeout")
     def grab(self, query):
-        self.focus = self.driver.find_element_by_css_selector(query)
+        self.focus = self.find_element(By.CSS_SELECTOR, query)
     def grab_x(self, xpath):
-        self.focus = self.driver.find_element_by_xpath(xpath)
-    def text(self, txt):
+        self.focus = self.find_element(By.XPATH, xpath)
+    def clear(self, _):
         self.focus.clear()
+    def text(self, txt):
         self.focus.send_keys(txt)
     def text_s(self, secret):
-        self.focus.clear()
         self.focus.send_keys(self.secrets[secret])
     def text_i(self, msg):
-        self.focus.clear()
         txt = input(msg)
         self.focus.send_keys(txt)
+    def select(self, idx):
+        idx = int(idx)
+        sel = Select(self.focus)
+        sel.select_by_index(idx)
+    def select_v(self, value):
+        sel = Select(self.focus)
+        sel.select_by_value(value)
+    def select_t(self, text):
+        sel = Select(self.focus)
+        sel.select_by_visible_text(text)
     def click(self, query):
-        self.driver.find_element_by_css_selector(query).click()
+        self.find_element(By.CSS_SELECTOR, query).click()
     def click_x(self, xpath):
-        self.driver.find_element_by_xpath(xpath).click()
+        self.find_element(By.XPATH, xpath).click()
     def enter(self, _):
         self.focus.send_keys(Keys.ENTER)
     def tab(self, _):
         self.focus.send_keys(Keys.TAB)
     def __del__(self):
         # save secrets
-        # with open(".secrets", "w") as f:
-        #     json.dump(f)
-        self.quit()
+        with open("secrets.json", "w") as f:
+            json.dump(self.secrets, f)
+        self.quit("")
     def session(self):
         self.driver = webdriver.Firefox()
         self.running = True
@@ -99,20 +131,21 @@ class Automat:
     def run_seq(self, seq):
         if not self.driver:
             o = Options()
-            o.add_argument("--headless")
+            #o.add_argument("--headless")
             self.driver = webdriver.Firefox(options=o)
+        for action in seq:
+            self.exec(action)
 
     def exec(self, action:str):
         command = action
         arg = ""
         idx = action.find(" ")
         if idx != -1:
-            action = action[:idx]
-            arg = action[idx+1:]
+            action = command[:idx]
+            arg = command[idx+1:]
         action = action.strip()
         arg = arg.strip()
         if action in self.commands:
-            print(action, arg)
             self.commands[action](arg)
         else:
             print(f"command '{action} not recognized'")
@@ -125,7 +158,7 @@ def main():
     if len(args) == 0:
         automat.session()
     else:
-        with open(args[1]) as f:
+        with open(args[0]) as f:
             seq = f.read().splitlines()
             automat.run_seq(seq)
 
